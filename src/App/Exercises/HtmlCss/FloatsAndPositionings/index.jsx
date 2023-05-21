5a2e5c5b543b61c8b5ec9abadbe5469fc5454a7c
KamilWerochowski <kwerochowski@gmail.com>
2023-05-21 10:02:14 +0200

    floats-poczatki

---
+++ src/App/Exercises/HtmlCss/FloatsAndPositionings/index.jsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+export function FloatsAndPositionings() {
+  return <div>Ćwiczenia z Pozycjonowania</div>;
+
+}
+<div>Lorem Ipsum</div>;
---
+++ src/App/Exercises/HtmlCss/FloatsAndPositionings/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { FloatsAndPositionings } from './index';
+
+export const blockRouterMetaData = {
+  path: '20-05-2023/floats',
+  date: '20-05-2023',
+  linkLabel: 'Floats',
+  blockNo: 1,
+  element: <FloatsAndPositionings />,
+  tags: ['float', 'posisioning'],
+};
